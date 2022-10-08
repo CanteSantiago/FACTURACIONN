c19124ee5191d6fb1da58c97f1d184256bd6d91d
CanteSantiago <cantesantiago656@gmail.com>
2022-10-08 10:21:00 -0500

    Commit 5
    Leccion 6- Herencia de superClases

--- facturacion/src/com/tuempresa/facturacion/modelo/Autor.java
+++ facturacion/src/com/tuempresa/facturacion/modelo/Autor.java
@@ -3,22 +3,14 @@ package com.tuempresa.facturacion.modelo;
 import java.util.*;
 
 import javax.persistence.*;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
 
-import org.hibernate.annotations.*;
 import org.openxava.annotations.*;
 
 import lombok.*;
 
 @Entity @Getter @Setter
-public class Autor {
+public class Autor extends Identificable{
 
-	@Id @GeneratedValue(generator = "system-uuid") @Hidden
-	@GenericGenerator (name = "system-uuid",strategy = "uuid")
-	@Column(length = 32)
-	String oid;
-	 
 	@Column(length = 50)
 	String nombre;
 	
@@ -26,4 +18,3 @@ public class Autor {
 	@ListProperties("numero, descripcion, precio")
 	Collection<Producto>productos;
 }
- 
